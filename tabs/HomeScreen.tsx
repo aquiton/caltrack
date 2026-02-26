bac6b294b8004fa17572f838794a4b00ce1a7753
Alex Quiton <aquiton22@gmail.com>
2026-02-25 23:53:22 -0500

    feat: wip macro tabs

--- tabs/HomeScreen.tsx
+++ tabs/HomeScreen.tsx
@@ -12,7 +12,27 @@ export const HomeScreen = () => {
         <Text style={styles.headerSubTitle}>Tue, Feb 24 . 2,100 cal goal</Text>
       </View>
       <CalorieRing current={current} goal={2750} />
-      <Button title="press" onPress={() => setCurrent(current + 10)} />
+      <View style={styles.macroContainer}>
+        <View style={styles.macroPill}>
+          <Text style={styles.macroProteinValue}>142g</Text>
+          <Text style={styles.macroProteinSubTitle}>PROTEIN</Text>
+          <View style={styles.macroProteinBar} />
+        </View>
+        <View style={styles.macroPill}>
+          <Text>186g</Text>
+          <Text>CARBS</Text>
+          <View />
+        </View>
+        <View style={styles.macroPill}>
+          <Text>48g</Text>
+          <Text>FATS</Text>
+          <View />
+        </View>
+      </View>
+      <View>
+        <Text>MEAL LOG</Text>
+      </View>
+      <View></View>
     </View>
   );
 };
@@ -38,5 +58,33 @@ const styles = StyleSheet.create({
     color: "#6b7494",
     fontSize: 16,
     fontWeight: 300,
+  },
+  macroContainer: {
+    flexDirection: "row",
+    gap: 8,
+    backgroundColor: "",
+  },
+  macroPill: {
+    backgroundColor: "#252a3a",
+    borderRadius: 16,
+    padding: 16,
+    flex: 1,
+    gap: 4,
+  },
+  macroProteinValue: {
+    color: "#4f8ef7",
+    fontWeight: 800,
+    fontSize: 18,
+  },
+  macroProteinSubTitle: {
+    color: "#3a4060",
+  },
+  macroProteinTrack: {},
+  macroProteinBar: {
+    backgroundColor: "#4f8ef7",
+    flex: 1,
+    height: 1,
+    padding: 2,
+    borderRadius: 4,
   },
 });
